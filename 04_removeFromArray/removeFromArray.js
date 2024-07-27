d46071a5848efa78062bf834d45df9bb0651506e
Tihomir Vitanov <vitanovtd@gmail.com>
2024-07-27 11:12:35 +0300

    tdd 04 task

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  //   let filterArray = array.filter((x) => x !== numberToBeRemoved);
+  const newArray = [];
+  // use forEach to go through the array
+  array.forEach((item) => {
+    // push every element into the new array
+    // UNLESS it is included in the function arguments
+    // so we create a new array with every item, except those that should be removed
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  // and return that array
+  return newArray;
 };
 
 // Do not edit below this line
