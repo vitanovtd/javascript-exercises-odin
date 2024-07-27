ae20a1fa5f7b5563d0802f7d6fade0fed9bc481a
Tihomir Vitanov <vitanovtd@gmail.com>
2024-07-27 10:59:56 +0300

    tdd second function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function (word) {
+  let wordArray = word.split("");
+  let reversedArray = wordArray.reverse().join("");
 
+  console.log(reversedArray);
+  return reversedArray;
 };
 
 // Do not edit below this line
